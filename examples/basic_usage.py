054a20e5d13a7d15a0979748a7f13cb125a94237
ff137 <ff137@proton.me>
2025-02-25 17:48:43 +0200

    :book: Add more examples

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -2,6 +2,27 @@
 
 import ohlc_toolkit
 
-df = ohlc_toolkit.read_ohlc_csv("data/btcusd_bitstamp_1min_latest.csv", timeframe="1m")
+df_1min = ohlc_toolkit.read_ohlc_csv(
+    "data/btcusd_bitstamp_1min_latest.csv", timeframe="1m"
+)
 
-print(df)
+print("Input 1-minute OHLC data:")
+print(df_1min)
+
+
+df_5min = ohlc_toolkit.transform_ohlc(df_1min, timeframe="5m")
+
+print("Transformed 5-minute OHLC data (updated every minute):")
+print(df_5min)
+
+
+df_1h = ohlc_toolkit.transform_ohlc(df_1min, timeframe="1h", step_size_minutes=10)
+
+print("Transformed 1-hour OHLC data (updated every 10 minutes):")
+print(df_1h)
+
+
+df_arb = ohlc_toolkit.transform_ohlc(df_1min, timeframe="1d2h3m", step_size_minutes=137)
+
+print("Arbitrary timeframe (1 day 2 hours 3 minutes) with step size 137 minutes:")
+print(df_arb)
