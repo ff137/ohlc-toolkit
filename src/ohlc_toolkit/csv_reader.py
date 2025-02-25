daa61a04adaf42e15649d2019dad007505d4ab9d
ff137 <ff137@proton.me>
2025-02-25 16:58:48 +0200

    :art: Configure default dtypes

--- src/ohlc_toolkit/csv_reader.py
+++ src/ohlc_toolkit/csv_reader.py
@@ -44,6 +44,11 @@ def read_ohlc_csv(
     if dtype is None:
         dtype = {
             "timestamp": "int32",
+            "open": "float32",
+            "high": "float32",
+            "low": "float32",
+            "close": "float32",
+            "volume": "float32",
         }
 
     read_csv_params = {
