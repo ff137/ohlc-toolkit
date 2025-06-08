502bd64af20aee33bd9810f48805b051ff4d6fc7
ff137 <ff137@proton.me>
2025-06-08 15:44:49 +0300

    :art: Better support for parsing or formatting minutes vs seconds

--- src/ohlc_toolkit/timeframes.py
+++ src/ohlc_toolkit/timeframes.py
@@ -49,14 +49,16 @@ TIME_UNITS = {
 }
 
 
-def parse_timeframe(timeframe: str) -> int:
-    """Convert a timeframe string (e.g., '1h', '4h30m', '1w3d7h14m') into total seconds.
+def parse_timeframe(timeframe: str, to_minutes: bool = True) -> int:
+    """Convert a timeframe string (e.g., '1h', '4h30m', '1w3d7h14m') into total minutes (or seconds).
 
     Arguments:
         timeframe (str): Human-readable timeframe.
+        to_minutes (bool, optional): Whether to return the result in minutes. Defaults to True. If False, result is
+                returned in seconds.
 
     Returns:
-        int: Total number of seconds.
+        int: Total number of minutes (or seconds if to_minutes is False).
 
     Raises:
         ValueError: If the format is invalid.
@@ -69,22 +71,39 @@ def parse_timeframe(timeframe: str) -> int:
     total_seconds = sum(
         int(amount) * TIME_UNITS[unit.lower()] for amount, unit in matches
     )
-    return total_seconds
+    return total_seconds // 60 if to_minutes else total_seconds
 
 
-def format_timeframe(seconds: int | str) -> str:
-    """Convert a total number of seconds into a human-readable timeframe string.
+def format_timeframe(
+    *,
+    minutes: int | str | None = None,
+    seconds: int | str | None = None,
+) -> str:
+    """Convert a total number of seconds or minutes into a human-readable timeframe string.
 
     Arguments:
-        seconds (int | str): Total number of seconds.
-            If a string is provided, it is assumed to be a valid timeframe string.
+        minutes (int | str, optional): Total number of minutes.
+        seconds (int | str, optional): Total number of seconds.
 
     Returns:
         str: Human-readable timeframe string (e.g., '1h', '4h30m').
 
+    Raises:
+        ValueError: If both or neither of minutes and seconds are provided.
+
     """
+    if minutes and seconds:
+        raise ValueError("One of 'minutes' or 'seconds' must be provided, not both.")
+
+    seconds = _parse_time_input(seconds, "seconds")
     if isinstance(seconds, str):
-        return seconds
+        return seconds  # Already formatted string
+
+    minutes = _parse_time_input(minutes, "minutes")
+    if isinstance(minutes, str):
+        return minutes  # Already formatted string
+
+    seconds = minutes * 60 if minutes else seconds
 
     if seconds in COMMON_TIMEFRAMES.values():
         # Return predefined common timeframes if found
@@ -133,3 +152,16 @@ def validate_timeframe(time_step: int, user_timeframe: int, logger: Logger):
             f"Note: Requested timeframe ({user_timeframe}s) is not a multiple "
             f"of the time step ({time_step}s); values may not be suitable."
         )
+
+
+def _parse_time_input(time_input: int | str, time_type: str) -> int | str:
+    """Parse seconds or minutes inputs to int, or return string if already formatted."""
+    if isinstance(time_input, str):
+        # If string given - already formatted strings should just be returned
+        if validate_timeframe_format(time_input):
+            return time_input
+        try:
+            return int(time_input)  # Otherwise, assume string integer was passed
+        except ValueError:
+            raise ValueError(f"Invalid {time_type} format: {time_input}") from None
+    return time_input
